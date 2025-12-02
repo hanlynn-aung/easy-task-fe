03740972254fc0650c58cc9079239d26823f0278
hanlynn-aung <hanlynnaung1997@gmail.com>
2025-12-03 00:15:16 +0630

    old version for ngfor, ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,20 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-user';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = this.users[0].id;
+  selectedUserId?: string;
 
   onSelectUser(id: string) {
-    console.log('User id : ', id);
     this.selectedUserId = id;
   }
 
